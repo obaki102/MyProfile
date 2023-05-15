8f610bf9a25fb06490519187e1461dfc66377cea
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2023-05-15 19:35:51 +0800

    Theme changes

--- src/Client/Shared/Themes/DefaultTheme.cs
+++ src/Client/Shared/Themes/DefaultTheme.cs
@@ -8,17 +8,17 @@ public static class DefaultTheme
             var defaultDarkPallete = defaultTheme.PaletteDark;
             var defaultLightPallete = defaultTheme.Palette;
             //Dark 
-            defaultDarkPallete.Dark = "#161b22";
-            defaultDarkPallete.Primary = "#EEEEEE";
-            defaultDarkPallete.AppbarBackground = "#22252B";
-            defaultDarkPallete.DarkContrastText = "#ecf2f8";
-            defaultDarkPallete.Background = "#22252B";
-            defaultDarkPallete.DrawerBackground = "#303841";
-            defaultDarkPallete.Surface = "#2B333B";
+            defaultDarkPallete.Dark = "#202020";
+            defaultDarkPallete.Primary = "#F5F5F5";
+            defaultDarkPallete.AppbarBackground = "#121212";
+            defaultDarkPallete.DarkContrastText = "#F5F5F5";
+            defaultDarkPallete.Background = "#121212";
+            defaultDarkPallete.DrawerBackground = "#2A2A2A";
+            defaultDarkPallete.Surface = "#2A2A2A";
             defaultDarkPallete.TextPrimary = "#ecf2f8";
             defaultDarkPallete.PrimaryContrastText = "161b22";
-            defaultDarkPallete.AppbarText = "#EEEEEE";
-            defaultDarkPallete.Tertiary = "#22252B";
+            defaultDarkPallete.AppbarText = "#F5F5F5";
+            defaultDarkPallete.Tertiary = "#121212";
       
 
             //Light
